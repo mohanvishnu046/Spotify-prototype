5d68c22e3666fe6de3305cc947bed0f598779e1f
mohan vishnu <mohanvishnu565@outlook.com>
2025-12-14 17:53:05 +0530

    actuator to monitor app

--- SpotifyUserService/src/main/java/com/UserDetails/SpotifyuserService/SpotifyuserServiceApplication.java
+++ SpotifyUserService/src/main/java/com/UserDetails/SpotifyuserService/SpotifyuserServiceApplication.java
@@ -3,6 +3,7 @@ package com.UserDetails.SpotifyuserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -12,5 +13,13 @@ public class SpotifyuserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpotifyuserServiceApplication.class, args);
 	}
+
+//out using any where created to know about bean annotation
+//	to find it in actuator/beans
+	@Bean
+	public UserService userDetailService(){
+		return new UserService();
+	}
+
 
 }
